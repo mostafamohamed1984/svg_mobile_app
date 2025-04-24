41fea0b540a5ba2a31b7c8cec17957a655e2ee36
Mustafa <mustafanazieer@gmail.com>
2025-04-24 04:52:38 +0200

    feat: features to claim items child table doctype, project claim doctype and fixing the create claim with items ratio client script

--- svg_mobile_app/svg_mobile_app/doctype/project_contractors/project_contractors.js
+++ svg_mobile_app/svg_mobile_app/doctype/project_contractors/project_contractors.js
@@ -1,26 +1,23 @@
 // Copyright (c) 2025, SVG and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Project Contractors", {
-// 	refresh(frm) {
-
-// 	},
-// });
-
 frappe.ui.form.on("Project Contractors", {
     refresh: function(frm) {
         
     },
     
     setup: function(frm) {
-        // Filter items in the child table based on the company in item_defaults
-        frm.set_query("item", "items", function() {
+        // Filter items in the child table based on the company
+        frm.fields_dict.items.grid.get_field('item').get_query = function() {
             return {
-                query: "svg_mobile_app.svg_mobile_app.doctype.project_contractors.project_contractors.get_items_by_company",
-                filters: {
-                    "company": frappe.defaults.get_user_default('company')
-                }
+                filters: [
+                    ["Item", "name", "in", 
+                        // Subquery to get items with matching company
+                        `(SELECT parent FROM \`tabItem Default\` 
+                          WHERE company = '${frappe.defaults.get_user_default('company')}')`
+                    ]
+                ]
             };
-        });
+        };
     }
 });

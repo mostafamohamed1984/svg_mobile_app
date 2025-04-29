c7ad352df0266799146ee3856fcab5ac690424c8
Mustafa <mustafanazieer@gmail.com>
2025-04-29 21:25:33 +0300

    fixing the function not being loaded in the main server

--- svg_mobile_app/svg_mobile_app/doctype/project_claim/__init__.py
+++ svg_mobile_app/svg_mobile_app/doctype/project_claim/__init__.py
@@ -1,2 +1,5 @@
 # project_claim package
 from __future__ import unicode_literals
+
+# Import the needed functions to expose them at the module level
+from svg_mobile_app.svg_mobile_app.doctype.project_claim.project_claim import get_available_invoice_balances

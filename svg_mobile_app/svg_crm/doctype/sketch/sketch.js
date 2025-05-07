301773b6e2c788efcc6f711b2c65b83ddc4f512b
mostafamohamed1984 <mustafnazieer@gmail.com>
2025-05-07 14:44:12 +0300

    feat: adding sketch engineering tasks and sketch requirements child table doctypes in the requirements tab in the sketch doctype

--- svg_mobile_app/svg_crm/doctype/sketch/sketch.js
+++ svg_mobile_app/svg_crm/doctype/sketch/sketch.js
@@ -6,3 +6,67 @@
 
 // 	},
 // });
+
+frappe.ui.form.on("Sketch", {
+	refresh(frm) {
+        // Initialize the requirement items cache
+        frm.requirement_items = [];
+        
+        // Populate the requirement items from the current form data
+        if (frm.doc.sketch_requirements && frm.doc.sketch_requirements.length) {
+            frm.doc.sketch_requirements.forEach(req => {
+                if (req.item) {
+                    frm.requirement_items.push(req.item);
+                }
+            });
+        }
+	},
+    
+    // Update requirement items when the requirements table changes
+    sketch_requirements_add: function(frm, cdt, cdn) {
+        setTimeout(() => refresh_requirement_items(frm), 500);
+    },
+    
+    sketch_requirements_remove: function(frm, cdt, cdn) {
+        refresh_requirement_items(frm);
+    }
+});
+
+// Helper function to refresh the requirement items list and update select options
+function refresh_requirement_items(frm) {
+    frm.requirement_items = [];
+    
+    if (frm.doc.sketch_requirements && frm.doc.sketch_requirements.length) {
+        frm.doc.sketch_requirements.forEach(req => {
+            if (req.item) {
+                frm.requirement_items.push(req.item);
+            }
+        });
+    }
+    
+    // Update the options for the requirement_item select field in engineering tasks
+    frm.fields_dict.sketch_engineering_tasks.grid.update_docfield_property(
+        'requirement_item', 
+        'options', 
+        ["\n"].concat(frm.requirement_items)
+    );
+    
+    // Force grid to refresh to show updated options
+    frm.fields_dict.sketch_engineering_tasks.grid.refresh();
+}
+
+frappe.ui.form.on("Sketch Engineering Tasks", {
+    form_render(frm, cdt, cdn) {
+        // Make sure the requirement_item field has the latest options when rendering a row
+        var row = locals[cdt][cdn];
+        var grid = frm.fields_dict.sketch_engineering_tasks.grid;
+        
+        if (frm.requirement_items && frm.requirement_items.length) {
+            grid.update_docfield_property(
+                'requirement_item',
+                'options',
+                ["\n"].concat(frm.requirement_items)
+            );
+        }
+    }
+});

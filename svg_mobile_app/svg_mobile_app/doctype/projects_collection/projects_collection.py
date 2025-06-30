c56a66d9277bb08788b24a8c0c543f48eb20cc90
Mustafa Mohamed <mustafanazieer@gmail.com>
2025-06-30 13:59:08 +0300

    features, changes and fixes

--- svg_mobile_app/svg_mobile_app/doctype/projects_collection/projects_collection.py
+++ svg_mobile_app/svg_mobile_app/doctype/projects_collection/projects_collection.py
@@ -1,9 +1,20 @@
 # Copyright (c) 2025, SVG and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
+import re
 from frappe.model.document import Document
 
 
 class ProjectsCollection(Document):
-	pass
+	def before_save(self):
+		# Extract numeric value from project_name for proper sorting
+		if self.project_name:
+			# Extract all digits from project_name
+			numbers = re.findall(r'\d+', str(self.project_name))
+			if numbers:
+				# Use the first (or largest) number found
+				self.numeric_sort_field = int(numbers[0])
+			else:
+				# If no numbers found, set to 0
+				self.numeric_sort_field = 0

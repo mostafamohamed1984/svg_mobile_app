51c5145e879aa33a03424a37e561d899ebb489cf
Mustafa Mohamed <mustafanazieer@gmail.com>
2025-07-13 10:24:27 +0300

    fixes

--- svg_mobile_app/svg_mobile_app/hooks.py
+++ svg_mobile_app/svg_mobile_app/hooks.py
@@ -6,4 +6,8 @@ scheduler_events = {
     "daily": [
         "svg_mobile_app.svg_mobile_app.doctype.remote_access.remote_access.send_expiration_reminders"
     ]
-} 
+}
+
+# Ignore links to specified DocTypes when deleting documents
+# This prevents "Cannot delete because X is linked with Y" errors
+ignore_links_on_delete = ["Project Claim", "Project Contractors", "Sales Invoice", "Employee Advance", "Project Advances"]

562da91080592cdcf7e0176a52af448b49cf57bf
Mustafa Mohamed <mustafanazieer@gmail.com>
2025-08-10 12:07:06 +0300

    adding some modifications to the checkin/out button in the desk's navbare

---
+++ svg_mobile_app/svg_mobile_app/doctype/navbar_checkin_company/navbar_checkin_company.py
@@ -0,0 +1,7 @@
+from frappe.model.document import Document
+
+
+class NavbarCheckinCompany(Document):
+    """Child table row holding a Company link for navbar checkin settings."""
+    pass
+
---
+++ svg_mobile_app/svg_mobile_app/doctype/navbar_checkin_role/navbar_checkin_role.py
@@ -0,0 +1,7 @@
+from frappe.model.document import Document
+
+
+class NavbarCheckinRole(Document):
+    """Child table row holding a Role link for navbar checkin settings."""
+    pass
+
